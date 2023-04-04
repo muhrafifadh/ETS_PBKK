5d7e30382441bef64b332b1695841498f1c07d62
muhrafifadh <maashal.rafif@gmail.com>
2023-04-04 12:31:25 +0700

    Add project files.

---
+++ WeatherApp/FrmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WeatherApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Transparent;
+            this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel7.Controls.Add(this.panel1);
+            this.panel7.Controls.Add(this.label5);
+            this.panel7.Controls.Add(this.label4);
+            this.panel7.ForeColor = System.Drawing.Color.Black;
+            this.panel7.Location = new System.Drawing.Point(54, 13);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(372, 306);
+            this.panel7.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::WeatherApp.Properties.Resources.image_13;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(117, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 121);
+            this.panel1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(116, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(142, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Few Clouds";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(46, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(283, 73);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "29,97 °C";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(119, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 42);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "11.11.11 A.M";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(149, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 39);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "01/01/2001";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(161, 474);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 39);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Surabaya";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::WeatherApp.Properties.Resources._1540002_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(468, 539);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmMain";
+            this.Load += new System.EventHandler(this.FrmMain_load);
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WeatherApp/FrmMain.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            GetCurrentWeather();
+        }
+        private void FrmMain_load(object sender, EventArgs e)
+        {
+            // Set up the label to display the current time
+            label1.Text = DateTime.Now.ToString("h:mm:ss tt");
+            label2.Text = DateTime.Now.ToString("MM/dd/yyyy");
+        }
+
+        private void GetCurrentWeather()
+        {
+            // Replace YOUR_API_KEY with your OpenWeatherMap API key
+            string apiKey = "e4e8d3d399369f3fff6ac11d5d6ec57e";
+
+            // Set the city name to Surabaya
+            string cityName = "Surabaya";
+
+            // Create an HTTP client to send a request to the OpenWeatherMap API
+            HttpClient client = new HttpClient();
+
+            // Send a GET request to the OpenWeatherMap API to get the current weather for Surabaya
+            HttpResponseMessage response = client.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric").Result;
+
+            // Read the response as a string
+            string responseContent = response.Content.ReadAsStringAsync().Result;
+
+            // Deserialize the response JSON into a WeatherData object using Newtonsoft.Json
+            WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(responseContent);
+
+            // Display the current weather in a label control
+            label4.Text = $"{weatherData.Main.Temp}°C";
+            label5.Text = $"{weatherData.Weather[0].Description}.";
+        }
+
+        // Define a class to represent the weather data returned by the OpenWeatherMap API
+        public class WeatherData
+        {
+            public MainData Main { get; set; }
+            public Weather[] Weather { get; set; }
+        }
+
+        public class MainData
+        {
+            public float Temp { get; set; }
+        }
+
+        public class Weather
+        {
+            public string Description { get; set; }
+        }
+
+    }
+}
